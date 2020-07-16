8593b1cc514fb494b09e0679fa1562cfe85d3482
DokterStrength <phvietan@gmail.com>
2020-07-16 21:24:08 +0700

    fix

--- xss/index.js
+++ xss/index.js
@@ -5,21 +5,12 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const fs = require('fs');
 
-app.use(function(req, res, next) {
-    res.setHeader("Content-Security-Policy", "script-src 'none'; object-src 'none';");
-    return next();
-});
-
-// Cookies 
-const secretSession = uuid() + uuid() + uuid();
 app.use(cookieParser());
-app.use(session({
-  secret: secretSession,
-  resave: true,
-  saveUninitialized: true,
-  name: 'id',
-}));
 
+// app.use(function(req, res, next) {
+//     res.setHeader("Content-Security-Policy", "script-src 'none'; object-src 'none';");
+//     return next();
+// });
 
 // Template
 app.engine('html', function (filePath, options, callback) { // define the template engine
@@ -38,14 +29,18 @@ app.set('views', './views') // specify the views directory
 app.set('view engine', 'html') // register the template engine
 
 app.get('/', (req, res) => {
+    let cookie = req.cookies.id;
+    if (!req.cookies.id) {
+        cookie = uuid()
+        res.cookie('id', cookie);
+    }
+
     const referer = req.header('Referer');
     const referer_dec = decodeURIComponent(referer);
 
-    console.log(referer_dec);
-
     return res.render("index.html", {
         referer: referer_dec,
-        id: req.session.id,
+        id: cookie,
     });
 });
 
